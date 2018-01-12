16b873a10a803881de5a84f3d42da4b928f021fb
Marcel Vroone <mvroone@it-actief.nl>
2018-01-12 23:20:51 +0100

    Added ILogger, removed registration of IEnumerable

--- src/Mondop.Abstractions/IoC/IoCContainer.cs
+++ src/Mondop.Abstractions/IoC/IoCContainer.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace Mondop.Abstractions.IoC
+﻿namespace Mondop.Abstractions.IoC
 {
     public interface IIoCContainer
     {
         void Register<TService, TInterface>();
         void RegisterSingleton<TServie, TInterface>();
-        void Register<TService, TInterface>(IEnumerable<TService> service);
     }
 }
---
+++ src/Mondop.Abstractions/Logging/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mondop.Abstractions.Logging
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Warn(string message);
+        void Warn(string message, Exception exception);
+        void Error(string message);
+        void Error(string message, Exception exception);
+        void Fatal(string message);
+        void Fatal(string message, Exception exception);
+    }
+}
