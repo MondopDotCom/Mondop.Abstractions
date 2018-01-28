e2371fe8ffa3359e20dea2458d306075efac2285
Marcel Vroone <mvroone@it-actief.nl>
2018-01-28 21:06:41 +0100

    Added RegisterCollection

--- src/Mondop.Abstractions/IoC/IoCContainer.cs
+++ src/Mondop.Abstractions/IoC/IoCContainer.cs
@@ -1,8 +1,13 @@
-﻿namespace Mondop.Abstractions.IoC
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mondop.Abstractions.IoC
 {
     public interface IIoCContainer
     {
         void Register<TService, TImplementation>() where TService: class where TImplementation: class, TService;
         void RegisterSingleton<TService, TImplementation>() where TService: class where TImplementation: class, TService;
+        void Register<TService>() where TService: class;
+        void RegisterCollection<TService>(IEnumerable<Type> implementations) where TService : class;
     }
 }
